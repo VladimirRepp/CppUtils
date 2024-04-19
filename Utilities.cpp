9f60aa24a78b8c43835da4aee2102f088f85a16e
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2024-04-19 20:23:58 +0700

    Add files via upload

---
+++ Utilities.cpp
@@ -0,0 +1,61 @@
+#include "Utilities.h"
+
+#pragma region Functions
+std::string ConvertToString(System::String^ s) {
+	using namespace Runtime::InteropServices;
+	const char* chars =
+		(const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+	std::string	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+
+	return os;
+}
+
+System::String^ ConvertToString(std::string& os) {
+	System::String^ s = gcnew System::String(os.c_str());
+
+	return s;
+}
+#pragma endregion
+
+#pragma region Messager WinForm
+void Messager::WarningMessage(String^ mess)
+{
+	MessageBox::Show(mess, "Внимание!");
+}
+
+void Messager::ExceptionMessage(String^ mess)
+{
+	MessageBox::Show(mess, "Вызвано исключение!");
+}
+
+void Messager::ExceptionMessage(Exception^ ex)
+{
+	MessageBox::Show(ex->Message, "Вызвано исключение!");
+}
+
+void Messager::Message(String^ mess, String^ header)
+{
+	MessageBox::Show(mess, header);
+}
+#pragma endregion
+
+#pragma region StringHelper
+std::string StringHelper::ConvertToString(System::String^ s)
+{
+	using namespace Runtime::InteropServices;
+	const char* chars =
+		(const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+	std::string	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+
+	return os;
+}
+
+String^ StringHelper::ConvertToString(std::string& os)
+{
+	System::String^ s = gcnew System::String(os.c_str());
+
+	return s;
+}
+#pragma endregion
