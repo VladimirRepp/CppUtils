e5663e496304fff86d738caf4480b8a338287bc4
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2024-04-26 22:09:22 +0700

    Update Encoder_CryptoAPI.h

--- Encoder_CryptoAPI.h
+++ Encoder_CryptoAPI.h
@@ -1,4 +1,7 @@
 #pragma once
+#define MY_ENCODING_TYPE  (PKCS_7_ASN_ENCODING | X509_ASN_ENCODING)
+
+#include "ConsoleHelper.h"
 
 #include <iostream>
 #include <windows.h>
@@ -6,26 +9,44 @@
 #include <stdio.h>
 #include <string>
 #include <fstream>
-#include "ConsoleHelper.h"
 using namespace std;
+
+#pragma comment(lib, "crypt32.lib")
+
+enum EStateKey {
+	Non,
+	Gen,
+	Import,
+	Export
+};
 
 /// <summary>
 /// Based On CryptoAPI
 /// </summary>
 class Encoder_CryptoAPI
 {
-	string path_to_session_key;
+private:
+	EStateKey stateKey;
+	bool isHandleMessage;
 
 	HCRYPTPROV hProv;
 	HCRYPTKEY hSessionKey;
 
-	void SaveSessionKey();
-	void LoadSessionKey();
+	bool ExportKey();
+	bool ImportKey();
+
+	void MyHandleMessage(string);
+	void MyHandleError(string);
 
 public:
-	Encoder_CryptoAPI();
-	Encoder_CryptoAPI(string path_to_session_key) : path_to_session_key(path_to_session_key) { hProv = NULL; hSessionKey = NULL; }
+	Encoder_CryptoAPI(EStateKey stateKey, bool isHandleMessage = false);
 	~Encoder_CryptoAPI();
+
+	bool SaveKey();
+	bool LoadKey();
+
+	void SetKey(HCRYPTKEY hSessionKey);
+	HCRYPTKEY GetKey() { return hSessionKey; }
 
 	string Encrypt(string source_str, bool isDebugPrint = false);
 	string Decrypt(string source_str, bool isDebugPrint = false);
@@ -40,11 +61,18 @@ void main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	Encoder encoder;
-	string coder_str = encoder.Encrypt("УниверПервый", true);
-	
+	Encoder_CryptoAPI encoder_en(EStateKey::Gen, true);
+	Encoder_CryptoAPI encoder_de(EStateKey::Non, true);
+
+	string coder_str = encoder_en.Encrypt("УниверПервый", true);
+	encoder_en.SaveKey();
+
+	system("pause");
 	cout << endl;
-	encoder.Decrypt(coder_str, true);
+	
+	//encoder_de.SetKey(encoder_en.GetKey());
+	encoder_de.LoadKey();
+	encoder_de.Decrypt(coder_str, true);
 
 	system("pause");
 	return;
