59ef34b8f6efc3ce1964be1695de70c9e74ed368
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2024-04-26 22:08:58 +0700

    Update Encoder_CryptoAPI.cpp

--- Encoder_CryptoAPI.cpp
+++ Encoder_CryptoAPI.cpp
@@ -1,55 +1,160 @@
 #include "Encoder_CryptoAPI.h"
 
-void Encoder_CryptoAPI::SaveSessionKey()
+bool Encoder_CryptoAPI::ExportKey()
 {
-	ofstream fout(path_to_session_key, ios::trunc | ios::binary);
-	fout << hSessionKey;
-	fout.close();
-	
-	cout << "Сеансовый ключ сохранен!" << endl;
+	HCRYPTKEY hPublicKey;
+
+	// Получение ключа для экспорта ключа шифрования
+	if (!CryptGetUserKey(hProv, AT_KEYEXCHANGE, &hPublicKey)) {
+		MyHandleError("Encoder_CryptoAPI::ExportKey(): CryptGetUserKey() is Fail!");
+		return false;
+	}
+
+	MyHandleMessage("Открытый ключ получен!");
+
+	DWORD count = 0;
+	// Получение размера массива, используемого для экспорта ключа
+	if (!CryptExportKey(hSessionKey, hPublicKey, PUBLICKEYBLOB, 0, NULL, &count)) {
+		MyHandleError("Encoder_CryptoAPI::ExportKey(): CryptExportKey() is Fail!");
+		return false;
+	}
+
+	// Инициализация массива, используемого для экспорта ключа
+	BYTE* data = static_cast<BYTE*>(malloc(count));
+	ZeroMemory(data, count);
+
+	// Экспорт ключа шифрования
+	if (!CryptExportKey(hSessionKey, hPublicKey, PUBLICKEYBLOB, 0, data, &count)) {
+		MyHandleError("Encoder_CryptoAPI::ExportKey(): CryptExportKey() is Fail!");
+		return false;
+	}
+
+	MyHandleMessage("Экспорт ключа завершен!");
+	return true;
 }
 
-void Encoder_CryptoAPI::LoadSessionKey()
+bool Encoder_CryptoAPI::ImportKey()
 {
-	ifstream fin(path_to_session_key, ios::binary);
-	fin >> hSessionKey;
-	fin.close();
+	HCRYPTKEY hPublicKey, hNewKey;
 
-	cout << "Сеансовый ключ загружен!" << endl;
+	// Получение ключа для импорта ключа шифрования
+	if (!CryptGetUserKey(hProv, AT_KEYEXCHANGE, &hPublicKey)) {
+		MyHandleError("Encoder_CryptoAPI::ImportKey(): CryptGetUserKey() is Fail!");
+		return false;
+	}
+	MyHandleMessage("Открытый ключ получен!");
+
+	DWORD count = 0;
+	// Получение размера массива, используемого для экспорта ключа
+	if (!CryptExportKey(hSessionKey, hPublicKey, PUBLICKEYBLOB, 0, NULL, &count)) {
+		MyHandleError("Encoder_CryptoAPI::ImportKey(): CryptExportKey() is Fail!");
+		return false;
+	}
+
+	// Инициализация массива, используемого для экспорта ключа
+	BYTE* data = static_cast<BYTE*>(malloc(count));
+	ZeroMemory(data, count);
+
+	// Импорт ключа шифрования из полученного массива данных
+	if (!CryptImportKey(hProv, data, count, hPublicKey, 0, &hNewKey)) {
+		MyHandleError("Encoder_CryptoAPI::ImportKey(): CryptImportKey() is Fail!");
+		return false;
+	}
+
+	MyHandleMessage("Импорт ключа завершен!");
+	hSessionKey = hNewKey;
+
+	return true;
 }
 
-Encoder_CryptoAPI::Encoder_CryptoAPI()
+void Encoder_CryptoAPI::MyHandleMessage(string message)
 {
-	path_to_session_key = "sk.bin";
+	if (!isHandleMessage)
+		return;
 
-	hSessionKey = NULL;
-	hProv = NULL;
+	cout << message << endl;
+}
+
+void Encoder_CryptoAPI::MyHandleError(string message)
+{
+	if (!isHandleMessage)
+		return;
+
+	cout <<"Ошибка! Сообщение: " << message << endl;
+}
+
+Encoder_CryptoAPI::Encoder_CryptoAPI(EStateKey stateKey, bool isHandleMessage)
+{
+	this->isHandleMessage = isHandleMessage;
+
+	// Получение контекста криптопровайдера
+	if (!CryptAcquireContext(&hProv, NULL, // TEXT("{EB57ED8A-CCCC-4bf5-8659-9DF2F05F24AD}")
+		NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+	{
+		MyHandleError("Encoder_CryptoAPI::Encoder_CryptoAPI(): CryptAcquireContext Is Fail!");
+		return;
+	}
+	MyHandleMessage("Провайдер криптографии инициализирован . . .");
+
+	if (stateKey == EStateKey::Gen) {
+		// Генерация сеансового ключа ключа
+	/*	if (!CryptGenKey(hProv, CALG_RC4,
+			CRYPT_EXPORTABLE | CRYPT_ENCRYPT | CRYPT_DECRYPT, &hSessionKey))*/
+
+		if (!CryptGenKey(hProv, CALG_RC4, CRYPT_EXPORTABLE, &hSessionKey))
+		{
+			MyHandleError("Encoder_CryptoAPI::Encoder_CryptoAPI(): CryptGenKey Is Fail!");
+			return;
+		}
+		MyHandleMessage("Сеансовый ключ сгенерирован . . .");
+	}
+	else if (stateKey == EStateKey::Import) {
+		// Импорт сеансового ключа
+		if (!ImportKey()) {
+			MyHandleError("Encoder_CryptoAPI::Encoder_CryptoAPI(): ImportKey Is Fail!");
+			return;
+		}
+	}
 }
 
 Encoder_CryptoAPI::~Encoder_CryptoAPI()
 {
+	if (hSessionKey)
+		CryptDestroyKey(hSessionKey);
+
+	if (hProv)
+		CryptReleaseContext(hProv, 0);
+}
+
+bool Encoder_CryptoAPI::SaveKey()
+{
+	//return ExportKey();
+
+	ofstream fout("sk.bit", ios::out | ios::binary);
+	fout << hSessionKey;
+	fout.close();
+
+	return true;
+}
+
+bool Encoder_CryptoAPI::LoadKey()
+{
+	//return ImportKey();
+
+	ifstream fin("sk.bit", ios::binary);
+	fin >> hSessionKey;
+	fin.close();
+
+	return true;
+}
+
+void Encoder_CryptoAPI::SetKey(HCRYPTKEY hSessionKey)
+{
+	this->hSessionKey = hSessionKey;
 }
 
 string Encoder_CryptoAPI::Encrypt(string source_str, bool isDebugPrint)
 {
-	// Получение контекста криптопровайдера
-	if (!CryptAcquireContext(&hProv, NULL, NULL,
-		PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
-	{
-		ConsoleHelper::WriteWarning("Encoder::Crypt(): CryptAcquireContext Is Fail!");
-		return string();
-	}
-	std::cout << "Провайдер криптографии инициализирован . . ." << std::endl;
-
-	// Генерация сессионного ключа
-	if (!CryptGenKey(hProv, CALG_RC4, CRYPT_EXPORTABLE, &hSessionKey))
-	{
-		ConsoleHelper::WriteWarning("Encoder::Crypt(): CryptGenKey Is Fail!");
-		return string();
-	}
-
-	std::cout << "Сеансовый ключ сгенерирован . . ." << std::endl;
-
 	// Данные для шифрования
 	string encrypt_txt = source_str;
 	DWORD count = source_str.length();
@@ -57,11 +162,11 @@ string Encoder_CryptoAPI::Encrypt(string source_str, bool isDebugPrint)
 	// Шифрование данных
 	if (!CryptEncrypt(hSessionKey, 0, true, 0, (BYTE*)encrypt_txt.c_str(), &count, strlen(encrypt_txt.c_str())))
 	{
-		ConsoleHelper::WriteWarning("Encoder::Crypt(): CryptEncrypt Is Fail!");
+		MyHandleError("Encoder::Encrypt(): CryptEncrypt Is Fail!");
 		return string();
 	}
 
-	std::cout << "Шифрование завершено!" << std::endl;
+	MyHandleMessage("Шифрование завершено!");
 
 	if (isDebugPrint) {
 		// Тестовый вывод на экран
@@ -74,26 +179,7 @@ string Encoder_CryptoAPI::Encrypt(string source_str, bool isDebugPrint)
 
 string Encoder_CryptoAPI::Decrypt(string source_str, bool isDebugPrint)
 {
-	// Получение контекста криптопровайдера
-	if (!CryptAcquireContext(&hProv, NULL, NULL,
-		PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
-	{
-		ConsoleHelper::WriteException("Encoder::Crypt(): CryptAcquireContext Is Fail!");
-		return string();
-	}
-	std::cout << "Провайдер криптографии инициализирован . . ." << std::endl;
-
-	// Загрузка сеансового ключа
-	try {
-		LoadSessionKey();
-	}
-	catch (exception ex) {
-		string message = "Encoder::Decrypt(): ";
-		message += ex.what();
-		throw new exception(message.c_str());
-	}
-
-	DWORD count1 = source_str.length();
+	DWORD count = source_str.length();
 	string decrypt_str = source_str;
 
 	if (isDebugPrint) {
@@ -102,12 +188,12 @@ string Encoder_CryptoAPI::Decrypt(string source_str, bool isDebugPrint)
 		cout << "Длина строки: " << source_str.length() << endl;
 	}
 
-	if (!CryptDecrypt(hSessionKey, 0, true, 0, (BYTE*)decrypt_str.c_str(), &count1))
+	if (!CryptDecrypt(hSessionKey, 0, true, 0, (BYTE*)decrypt_str.c_str(), &count))
 	{
-		ConsoleHelper::WriteWarning("Encoder::Crypt(): CryptDecrypt Is Fail!");
+		MyHandleError("Encoder::Decrypt(): CryptDecrypt Is Fail!");
 		return string();
 	}
-	
+
 	if (isDebugPrint) {
 		// Тестовый вывод на экран
 		cout << "Расшифрованная строка: " << decrypt_str << endl;
