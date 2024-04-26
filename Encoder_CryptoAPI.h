36f611419399d060c9d68e51eea1081fa2ce9782
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2024-04-27 00:13:12 +0700

    Update Encoder_CryptoAPI.h

--- Encoder_CryptoAPI.h
+++ Encoder_CryptoAPI.h
@@ -22,6 +22,7 @@ enum EStateKey {
 
 /// <summary>
 /// Based On CryptoAPI
+/// - debug v0.1
 /// </summary>
 class Encoder_CryptoAPI
 {
@@ -50,6 +51,9 @@ public:
 
 	string Encrypt(string source_str, bool isDebugPrint = false);
 	string Decrypt(string source_str, bool isDebugPrint = false);
+
+	bool EncryptContentOfFile(string path_to_source_file, string path_to_result_file, bool isHasKeyInFile = false);
+	bool DecryptContentOfFile(string path_to_source_file, string path_to_result_file, bool isHasKeyInFile = false);
 };
 
 /*
@@ -61,20 +65,35 @@ void main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	Encoder_CryptoAPI encoder_en(EStateKey::Gen, true);
-	Encoder_CryptoAPI encoder_de(EStateKey::Non, true);
+	// string encoder
+	{
+		Encoder_CryptoAPI encoder_en(EStateKey::Gen, true);
+		Encoder_CryptoAPI encoder_de(EStateKey::Non, true);
 
-	string coder_str = encoder_en.Encrypt("УниверПервый", true);
-	encoder_en.SaveKey();
+		string coder_str = encoder_en.Encrypt("УниверПервый", true);
+		encoder_en.SaveKey();
 
-	system("pause");
-	cout << endl;
-	
-	//encoder_de.SetKey(encoder_en.GetKey());
-	encoder_de.LoadKey();
-	encoder_de.Decrypt(coder_str, true);
+		system("pause");
+		cout << endl;
 
-	system("pause");
+		//encoder_de.SetKey(encoder_en.GetKey());
+		encoder_de.LoadKey();
+		encoder_de.Decrypt(coder_str, true);
+		system("pause");
+	}
+
+	// File encoder
+	{
+		Encoder_CryptoAPI encoder_en(EStateKey::Gen, true);
+		Encoder_CryptoAPI encoder_de(EStateKey::Non, true);
+
+		encoder_en.EncryptContentOfFile("DbStudents.bin", "EncryptStudents.txt", true);
+		system("pause");
+
+		encoder_de.DecryptContentOfFile("EncryptStudents.txt", "DecryptStudents.txt", true);
+		system("pause");
+	}
+
 	return;
 }
 */
