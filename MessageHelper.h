00c5588ca85021db7dedfeb651da366b0d04e7d6
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2024-06-03 10:22:07 +0700

    Update and rename Messager.h to MessageHelper.h

--- MessageHelper.h
+++ MessageHelper.h
@@ -2,7 +2,7 @@
 using namespace System;
 using namespace System::Windows::Forms;
 
-namespace Messager {
+namespace MessageHelper {
 	void Show(String^ message, String^ header);
 	void ShowWarning(String^ message);
 	void ShowError(String^ message);
